5e393d3d1822bf10839c6e7e8e82ace5c9a5cac1
Andrew Berriault <berriaak@mcmaster.ca>
2020-01-08 12:41:02 -0500

    JS Basics Objects Complete.

---
+++ JS Basics/Objects/movieDB.js
@@ -0,0 +1,17 @@
+var movies = [
+    {
+        title: "Movie A",
+        rating: "5 stars",
+        hasWatched: true,
+    },
+    {
+        title: "Movie B",
+        rating: "2.5 stars",
+        hasWatched: true,
+    },
+        {
+        title: "Movie 3",
+        rating: "1 stars",
+        hasWatched: false,
+    }
+];
